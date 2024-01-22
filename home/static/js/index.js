f1228f223c6bcbc51d743cfdf763987acb5636ff
Jackson Lorran <computadordb@gmail.com>
2024-01-22 20:44:45 -0300

    adicionando efeito projects

--- home/static/js/index.js
+++ home/static/js/index.js
@@ -1,13 +1,17 @@
-var numeroEstrelas =400
+var numeroEstrelas =800
 const main = document.querySelector('.main')
 const white = 'white'
 const yellow = 'yellow'
 
 
 for (let i = 0; i < numeroEstrelas; i++) {
-  const estrela = document.createElement('div')
-  estrela.style.left = Math.random() * window.innerWidth + 'px'
-  estrela.style.top = Math.random() * window.innerHeight + 'px'
+  //como eu faco para que as estrelas ocupem 100% da tela?
+
+  //
+  const estrela = document.createElement('span')
+  estrela.style.right = Math.random() * window.innerWidth + 'px'
+  estrela.style.top = Math.random() * document.documentElement.scrollHeight + 'px'
+
   estrela.style.backgroundColor = (Math.random() > 0.2) ? white : yellow
   estrela.className = 'stars'
   main.appendChild(estrela)
